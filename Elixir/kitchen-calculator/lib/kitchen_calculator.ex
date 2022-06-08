464983021276f4664a9e319707f37e872677d317
LuizF6856 <luizfellipe6843@gmail.com>
2022-06-08 10:27:52 -0300

    refatorando

--- Elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ Elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,47 +1,21 @@
 defmodule KitchenCalculator do
   # Unit to convert in milliters
-  @ml 1
-  @cup 240
-  @fluid_ounce 30
-  @teaspoon 5
-  @tablespoon 15
-
+  @unit %{
+    milliliter: 1,
+    cup: 240,
+    fluid_ounce: 30,
+    teaspoon: 5,
+    tablespoon: 15
+  }
   def get_volume(volume_pair), do: elem(volume_pair, 1)
 
-  def to_milliliter({:milliliter, _volume} = volume_pair) do
-    {:milliliter, get_volume(volume_pair) * @ml}
+  def to_milliliter({atomo, _volyme} = volume_pair) do
+    {:milliliter, get_volume(volume_pair) * @unit[atomo]}
   end
 
-  def to_milliliter({:cup, _volume} = volume_pair) do
-    {:milliliter, get_volume(volume_pair) * @cup}
+  def from_milliliter(volume_pair, atomo) do
+    {atomo, get_volume(volume_pair) / @unit[atomo]}
   end
-
-  def to_milliliter({:fluid_ounce, _volume} = volume_pair) do
-    {:milliliter, get_volume(volume_pair) * @fluid_ounce}
-  end
-
-  def to_milliliter({:teaspoon, _volume} = volume_pair) do
-    {:milliliter, get_volume(volume_pair) * @teaspoon}
-  end
-
-  def to_milliliter({:tablespoon, _volume} = volume_pair) do
-    {:milliliter, get_volume(volume_pair) * @tablespoon}
-  end
-
-  def from_milliliter(volume_pair, :milliliter = unit),
-    do: {unit, get_volume(volume_pair) / @ml}
-
-  def from_milliliter(volume_pair, :cup = unit),
-    do: {unit, get_volume(volume_pair) / @cup}
-
-  def from_milliliter(volume_pair, :fluid_ounce = unit),
-    do: {unit, get_volume(volume_pair) / @fluid_ounce}
-
-  def from_milliliter(volume_pair, :teaspoon = unit),
-    do: {unit, get_volume(volume_pair) / @teaspoon}
-
-  def from_milliliter(volume_pair, :tablespoon = unit),
-    do: {unit, get_volume(volume_pair) / @tablespoon}
 
   def convert(volume_pair, unit) do
     volume_pair
